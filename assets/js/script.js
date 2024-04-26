2e759cb1efe07b4b4a5cf3c168c4ce548967f91d
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-04-26 16:22:01 +0600

    Some bugs are fixed & New Features Unlocked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,15 @@ const downloadBtn = document.querySelector(".download");
 const input = document.querySelector(".title input");
 const image = document.querySelector(".image");
 const container = document.querySelector(".container");
+const toast = document.querySelector(".tost-notification");
 
 generateBtn.addEventListener("click", () => {
   if (input.value === "") {
     container.classList.add("error");
+    toast.style.display = "flex";
     image.innerHTML = "";
   } else {
+    toast.style.display = "none";
     container.classList.remove("error");
     const qrCodeImageURL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
       input.value
@@ -25,3 +28,7 @@ generateBtn.addEventListener("click", () => {
 setInterval(() => {
   container.classList.remove("error");
 }, 1500);
+
+setInterval(() => {
+  toast.style.display = "none";
+}, 2500);
