f9698e188954cd193de754ae53443354830dfe6c
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-04-27 11:37:45 +0600

    Some Bugs Fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,31 @@ const downloadBtn = document.querySelector(".download");
 const input = document.querySelector(".title input");
 const image = document.querySelector(".image");
 const container = document.querySelector(".container");
-const toast = document.querySelector(".tost-notification");
+
+const showToast = (message) => {
+  const toast = document.querySelector(".tost-notification");
+  const toastContent = document.querySelector(".tost-notification p");
+
+  toast.style.display = "flex";
+  toastContent.innerText = message;
+  setTimeout(() => (toast.style.display = "none"), 2500);
+};
 
 generateBtn.addEventListener("click", () => {
-  if (input.value === "") {
+  if (!input.value) {
     container.classList.add("error");
-    toast.style.display = "flex";
     image.innerHTML = "";
+    showToast("At first, type anything");
+    setTimeout(() => container.classList.remove("error"), 1500);
   } else {
-    toast.style.display = "none";
     container.classList.remove("error");
     const qrCodeImageURL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
       input.value
     )}`;
-
-    // Display QR code image
     image.innerHTML = `<div class="img">
-      <img src="${qrCodeImageURL}" alt="QR Code" />
-    </div>`;
+    <img src="${qrCodeImageURL}" alt="QR Code" />
+  </div>`;
+    showToast(`Your Text with "${input.value}" is generated in QR Code`);
     input.value = "";
   }
 });
-
-setInterval(() => {
-  container.classList.remove("error");
-}, 1500);
-
-setInterval(() => {
-  toast.style.display = "none";
-}, 2500);
